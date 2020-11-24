e9a928417fb50404f0b339323ee81b9e0f716a7b
Maaroufi-khaled <maaroufikhaled06@gmail.com>
2020-11-24 10:48:41 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,40 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 65,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6,
+
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 8,
+            countInStock: 5,
+
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 9,
+            countInStock: 3,
+
+        },
+    ]
+}
