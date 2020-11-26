38b66772eb60a76abec1672742baeaf7e57cd8f8
Maaroufi-khaled <maaroufikhaled06@gmail.com>
2020-11-26 13:46:00 +0100

    walid

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const { getToken } = require('../util');
+const { isAuth, isAdmin } = require('../util');
 const Product = require('../models/productModel');
+
 const router = express.Router();
+
 router.get('/', async (req, res) => {
   const products = await Product.find({});
   res.send(products);
 });
-router.post('/', async (req, res) => {
+
+router.get('/:id', async (req, res) => {
+  const product = await Product.findOne({ _id: req.params.id });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found." })
+  }
+});
+
+
+
+
+router.post('/', isAuth, isAdmin, async (req, res) => {
   const product = new Product({
     name: req.body.name,
     price: req.body.price,
@@ -26,4 +41,34 @@ router.post('/', async (req, res) => {
   }
   return res.status(500).send({ message: ' Error in Creating Product.' });
 });
+router.put('/:id', isAuth, isAdmin, async (req, res) => {
+  const productId = req.params.id;
+  const product = await Product.findById({ _id: productId });
+  if (product) {
+    product.name = req.body.name;
+    product.price = req.body.price;
+    product.image = req.body.image;
+    product.brand = req.body.brand;
+    product.category = req.body.category;
+    product.countInStock = req.body.countInStock;
+    product.description = req.body.description;
+    const updatedProduct = await product.save();
+    if (updatedProduct) {
+      return res
+        .status(200)
+        .send({ message: 'Product Updated', data: updatedProduct });
+    }
+
+  }
+  return res.status(500).send({ message: ' Error in Updating Product.' });
+});
+router.delete("/:id", isAuth, isAdmin, async (req, res) => {
+  const deletedProduct = await Product.findById(req.params.id);
+  if (deletedProduct) {
+    await deletedProduct.remove();
+    res.send({ message: 'Product Deleted' });
+  } else
+    res.send("Error in Deletion.")
+})
 module.exports = router;
+
--- util.js
+++ util.js
@@ -16,22 +16,31 @@ exports.getToken = (user) => {
   );
 };
 exports.isAuth = (req, res, next) => {
+  console.log('1')
   const token = req.headers.authorization;
   if (token) {
+    console.log('2')
     const onlyToken = token.slice(7, token.length);
     jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
       if (err) {
+        console.log('3')
         return res.status(401).send({ msg: 'Invalid Token' });
       }
-      req.user = token;
+      console.log('4')
+      req.user = decode;
       next();
       return;
     });
+  } else {
+    console.log('5')
+    return res.status(401).send({ msg: 'Token is not supplied.' });
   }
-  return res.status(401).send({ msg: 'Token is not supplied.' });
+
 };
 exports.isAdmin = (req, res, next) => {
+  console.log('success')
   if (req.user && req.user.isAdmin) {
+
     return next();
   }
   return res.status(401).send({ msg: 'Asmin Token is not valid.' });
